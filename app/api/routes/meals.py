d0dee7c308ab5c9d4c5fee21b25be38642134c2f
abimmost <atsimbomgwe31@outlook.com>
2026-06-09 23:23:50 +0100

    mod: removed redundant 'app' folder and contents

--- app/api/routes/meals.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Depends
-# from app.api.dependencies import get_db
-
-router = APIRouter()
-
-@router.get("/")
-async def get_meals():
-    return {"message": "List of meals"}
